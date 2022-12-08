96fecd4e6329cd159ccfca5eb9ef84b9c34c8fc0
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2022-12-08 17:51:12 -0500

    repo updates

--- Calculator.js
+++ Calculator.js
@@ -1,83 +1,109 @@
+/* Left to work on 
+- Simplify output to only display certain number of digits
+- Clear window unless operator is pressed, then compound
+- PEMDAS Functionality
+  - Add Parenthesis button
+- Negative Number Functionality
+  - Arithmetic with negative numbers
+  - +/- button functionality
+- Percentage function
+*/
+
 let calculator = document.querySelector('.calcBody')
 let calcButtons = document.querySelector('.calcButtons')
 let inputWindow = document.querySelector('.inputWindow')
+let clearWindow = false
+  // 4-4
+  // 2+(2*4)
+  let calculate = (input) => {
+    console.log(input)
+    // if the input does not contain an operator
+    if (
+      !input.includes('+') &&
+      !input.includes('-') &&
+      !input.includes('*') &&
+      !input.includes('/')
+    ) {
+      return input
+      // so it doesn't loop forever and brick my computer
+      // PEMDAS but lowkey fogetting PE for now
+      // if split only has 1 element, there's is nothing to be added
+    }
+    let split = input.split('+');
+    if (split.length !== 1) {
+      let firstValue = split[0]
+      let secondValue = split[1]
+      let newValue = parseFloat(firstValue) + parseFloat(secondValue) + '';
+      //This is a method, that replaces the old value, with the calculated value
+      input = input.replace(`${firstValue}+${secondValue}`, newValue)
+    }
+    split = input.split('-');
+    if (split.length !== 1) {
+      let firstValue = split[0]
+      let secondValue = split[1]
+      let newValue = parseFloat(firstValue) - parseFloat(secondValue) + '';
+      //This is a method, that replaces the old value, with the calculated value
+      input = input.replace(`${firstValue}-${secondValue}`, newValue)
+    }
+    split = input.split('/');
+    if (split.length !== 1) {
+      let firstValue = split[0]
+      let secondValue = split[1]
+      let newValue = parseFloat(firstValue) / parseFloat(secondValue) + '';
+      //This is a method, that replaces the old value, with the calculated value
+      input = input.replace(`${firstValue}/${secondValue}`, newValue)
+    }
+    split = input.split('*');
+    if (split.length !== 1) {
+      let firstValue = split[0]
+      let secondValue = split[1]
+      let newValue = parseFloat(firstValue) * parseFloat(secondValue) + '';
+      //This is a method, that replaces the old value, with the calculated value
+      input = input.replace(`${firstValue}*${secondValue}`, newValue)
+    }
+    // now i have to replace the first instance of operation 
 
-// 4-4
-// 2+(2*4)
-let calculate = (input) => {
-  // if the input does not contain an operator
-  if (
-    !input.includes('+') &&
-    !input.includes('-') &&
-    !input.includes('*') &&
-    !input.includes('/')
-  ) {
-    return input
-    // so it doesn't loop forever and brick my computer
-    // PEMDAS but lowkey fogetting PE for now
-    // if split only has 1 element, there's is nothing to be added
-  }
-  let split = inputWindow.textContent.split('+');
-  if (split.length !== 1) {
-    let firstValue = split[0]
-    let secondValue = split[1]
-    input = parseFloat(firstValue) + parseFloat(secondValue);
-  }
-  split = inputWindow.textContent.split('-');
-  if (split.length !== 1) {
-    let firstValue = split[0]
-    let secondValue = split[1]
-    input = parseFloat(firstValue) - parseFloat(secondValue);
-  }
-  split = inputWindow.textContent.split('/');
-  if (split.length !== 1) {
-    let firstValue = split[0]
-    let secondValue = split[1]
-    input = parseFloat(firstValue) / parseFloat(secondValue);
-  }
-  split = inputWindow.textContent.split('*');
-  if (split.length !== 1) {
-    let firstValue = split[0]
-    let secondValue = split[1]
-    input = parseFloat(firstValue) * parseFloat(secondValue);
-  }
-  // now i have to replace the first instance of operation 
-  
-  /*if (operator === 'add') {
-    result = parseFloat(n1) + parseFloat(n2)
-  } else if (operator === 'minus') {
-    result = parseFloat(n1) - parseFloat(n2)
-  } else if (operator === 'multiply') {
-    result = parseFloat(n1) * parseFloat(n2)
-  } else if (operator === 'divide') {
-    result = parseFloat(n1) / parseFloat(n2)
-  }*/
+    /*if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'minus') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }*/
 
-  return calculate(input.toString())
-}
+    return calculate(input)
+  }
 
+//add to clear after equals, last clicked button === 'equals'
 
 calcButtons.addEventListener('click', e => {
   if (e.target.matches('button')) {
     let key = e.target
     let action = key.dataset.action
     let keyContent = key.textContent
-    let inputWindowNum = inputWindow.textContent
     let previousKeyType = calculator.dataset.previousKeyType
 
     Array.from(key.parentNode.children)
       .forEach(k => k.classList.remove('pushedButton'))
+    
+    if (clearWindow){
+      inputWindow.textContent = ''
+      clearWindow = false
+      console.log(clearWindow)
+    }
 
     if (!action) {
-      if (inputWindowNum === '0' || previousKeyType === 'operator') {
+      if (inputWindow.textContent === '0' || previousKeyType === 'operator') {
         inputWindow.textContent = keyContent
       } else {
-        inputWindow.textContent = inputWindowNum + keyContent
+        inputWindow.textContent = inputWindow.textContent + keyContent
       }
     }
 
     if (action === 'decimal') {
-      inputWindow.textContent = inputWindowNum + '.'
+      inputWindow.textContent += '.'
     }
 
     if (
@@ -108,7 +134,18 @@ calcButtons.addEventListener('click', e => {
 
 
       inputWindow.textContent = calculate(inputWindow.textContent)
+      // if (
+      //   !input.includes('+') &&
+      //   !input.includes('-') &&
+      //   !input.includes('*') &&
+      //   !input.includes('/')
+      // ){
+      clearWindow = true
+      // }
+      //need to add in clear unless operator is entered
     }
   }
 })
+
+
 
